c78a29709c942e0abb0a0ec8379be3c75cc31671
Cheng Huzi <tigeraus@icloud.com>
2022-02-05 12:53:06 -0500

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dbabget",
-    version="0.0.2",
+    version="0.1",
     author="huzi",
     packages=find_packages(),
     entry_points={
